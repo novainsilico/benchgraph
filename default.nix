2d049c60356f833f1d377fb75bee0638c0c2a33e
Theophane Hufschmitt <theophane.hufschmitt@novadiscovery.com>
2018-08-21 08:55:38 +0000

    Resolve "Export bench results in a useful way"

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let p = nixpkgs; in
+
+let
+  rWrapper = p.rWrapper.override {
+    packages = [
+      p.rPackages.shiny p.rPackages.jsonlite
+      p.rPackages.ggplot2 p.rPackages.plotly
+      p.rPackages.shinyWidgets
+    ];
+  };
+in
+
+p.stdenv.mkDerivation {
+  name = "benchgraph";
+  src = ./.;
+
+  buildInputs = [ rWrapper ];
+
+  buildPhase = ''
+    cat <<EOF > benchgraph
+    #!/bin/sh
+
+    exec ${rWrapper}/bin/Rscript $out/server.R "\$@"
+    EOF
+    chmod +x benchgraph
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp benchgraph $out/bin/
+    cp server.R $out/
+  '';
+}
---
+++ server.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+loadBenchmarks <- function(inFile) {
+  benchs <- jsonlite::stream_in(file(inFile))
+  return (benchs[order(benchs$timestamp),])
+}
+
+args = commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+    inputFile = "benchs.json"
+} else {
+    inputFile = args[1]
+}
+
+benchs <- loadBenchmarks(inputFile)
+allBenchNames <- unique (benchs["bench_name"][,1])
+
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("Simwork benchmarks"),
+
+  sidebarLayout (
+      sidebarPanel(
+          shinyWidgets::pickerInput(
+              "enabledBenchmarkNames",
+              "Enabled benchmarks",
+              allBenchNames,
+              multiple = TRUE,
+              options = list(
+                  `actions-box` = TRUE,
+                  `live-search` = TRUE,
+                  `liveSearchNormalize` = TRUE
+              ),
+          )
+      ),
+      mainPanel(
+          plotlyOutput(outputId = "plot")
+
+      )
+  )
+)
+
+server <- function(input, output) {
+    output$plot <- renderPlotly ({
+        enabledBenchmarkNames <- input$enabledBenchmarkNames
+        displayedBenchs = benchs[benchs$bench_name %in% enabledBenchmarkNames, ]
+        if (length(enabledBenchmarkNames) > 0) {
+            ggplot(
+                displayedBenchs,
+                aes(
+                    commit_rev,
+                    time_in_nanos,
+                    group=bench_name,
+                    color=bench_name
+                )) +
+                theme(axis.text.x = element_text(angle = 45)) +
+                geom_line() +
+                geom_point()
+        } else {
+            ggplot()
+        }
+    })
+}
+
+app <- shinyApp(ui, server)
+runApp(app, launch.browser = F, host="0.0.0.0", port=8123)
